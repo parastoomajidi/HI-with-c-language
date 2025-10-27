472c40a213d58e30a8415ef53968df6d5ed82562
Parastoo Majidi <61692804+parastoomajidi@users.noreply.github.com>
2025-10-27 06:31:46 +0000

    add string type

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # HI-with-c-language
 here is how say hi in c language with array type. haha funny=)))))))))
 ![alt text](image.png)
+
+<!-- hi with int type -->
+![alt text](image-2.png)
+
+<!-- hi with String type -->
+
+![alt text](image-1.png)
--- hi.c
+++ hi.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main (void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-    // printf("%c%c%c\n", c1, c2, c3);
-    // here is if we change it to int
-    printf ("%i%i%i\n", c1,c2,c3);
+        // char c1 = 'H';
+        // char c2 = 'I';
+        // char c3 = '!';
+        // // printf("%c%c%c\n", c1, c2, c3);
+        // // here is if we change it to int
+        // printf ("%i%i%i\n", c1,c2,c3);
+    // in here check the hi with string type
+    string s = "HI !";
+    printf("%s\n", s);
+
+
 }
 
